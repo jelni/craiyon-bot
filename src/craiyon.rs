6ec1eaed7b9f041530e201f0defa51809311a06d
JelNiSław <jelnislaw@gmail.com>
2022-08-17 15:16:35 +0200

    Move API wrappers into single files

--- src/craiyon.rs
+++ src/craiyon.rs
@@ -1,4 +1,75 @@
-mod api;
-mod models;
+use std::time::{Duration, Instant};
 
-pub use api::*;
+use reqwest::StatusCode;
+use serde::{Deserialize, Serialize};
+
+const RETRY_COUNT: usize = 3;
+
+#[derive(Serialize)]
+struct Payload {
+    pub prompt: String,
+}
+
+#[derive(Deserialize)]
+struct Response {
+    pub images: Vec<String>,
+}
+
+pub struct GeneratedResult {
+    pub images: Vec<Vec<u8>>,
+    pub duration: Duration,
+}
+
+pub async fn generate<S: Into<String>>(
+    http_client: reqwest::Client,
+    prompt: S,
+) -> reqwest::Result<GeneratedResult> {
+    let body = Payload {
+        prompt: prompt.into(),
+    };
+    let mut retry = 0;
+    let (response, duration) = loop {
+        retry += 1;
+        let start = Instant::now();
+        match http_client
+            .post("https://backend.craiyon.com/generate")
+            .json(&body)
+            .send()
+            .await?
+            .error_for_status()
+        {
+            Ok(response) => {
+                break {
+                    let duration = start.elapsed();
+                    (response.json::<Response>().await?, duration)
+                }
+            }
+            Err(err) => {
+                let status = err.status();
+                if let Some(status) = status {
+                    log::warn!("HTTP error: {status}");
+                };
+                if retry < RETRY_COUNT {
+                    let duration = if status == Some(StatusCode::TOO_MANY_REQUESTS) {
+                        10
+                    } else {
+                        2
+                    };
+                    tokio::time::sleep(Duration::from_secs((retry * duration) as _)).await;
+                    log::info!("Retrying ({retry})…");
+                    continue;
+                }
+                log::warn!("Failed after {retry} retries");
+                return Err(err);
+            }
+        };
+    };
+
+    let images = response
+        .images
+        .into_iter()
+        .map(|data| base64::decode(data.replace('\n', "")).unwrap())
+        .collect();
+
+    Ok(GeneratedResult { images, duration })
+}
--- src/urbandictionary.rs
+++ src/urbandictionary.rs
@@ -1,4 +1,122 @@
-mod api;
-mod models;
+use std::fmt;
 
-pub use api::*;
+use reqwest::{StatusCode, Url};
+use serde::Deserialize;
+use teloxide::utils::markdown;
+use time::format_description::FormatItem;
+use time::macros::format_description;
+use time::OffsetDateTime;
+
+const DATETIME_FORMAT: &[FormatItem] = format_description!("[year]-[month]-[day]");
+
+#[derive(Deserialize)]
+struct Response {
+    pub list: Vec<Definition>,
+}
+
+#[derive(Deserialize)]
+pub struct Definition {
+    pub word: String,
+    pub definition: String,
+    pub example: String,
+    pub author: String,
+    #[serde(with = "time::serde::iso8601")]
+    pub written_on: OffsetDateTime,
+    pub thumbs_up: usize,
+    pub thumbs_down: usize,
+    pub permalink: String,
+}
+
+async fn search<S: AsRef<str>>(http_client: reqwest::Client, term: S) -> reqwest::Result<String> {
+    let response = http_client
+        .get(
+            Url::parse_with_params(
+                "https://www.urbandictionary.com/define.php",
+                [("term", term.as_ref())],
+            )
+            .unwrap(),
+        )
+        .send()
+        .await?
+        .error_for_status()?;
+
+    match response.status() {
+        StatusCode::FOUND => {
+            let (_, term) = response.headers()["Location"]
+                .to_str()
+                .unwrap()
+                .split_once("?term=")
+                .unwrap();
+
+            Ok(term.to_string())
+        }
+        StatusCode::OK => Ok(term.as_ref().to_string()),
+        _ => unreachable!(),
+    }
+}
+
+pub async fn define<S: AsRef<str>>(
+    http_client: reqwest::Client,
+    term: S,
+) -> reqwest::Result<Option<Definition>> {
+    let term = search(http_client.clone(), term).await?;
+    let mut definitions = http_client
+        .get(
+            Url::parse_with_params(
+                "https://api.urbandictionary.com/v0/define",
+                [("term", term)],
+            )
+            .unwrap(),
+        )
+        .send()
+        .await?
+        .error_for_status()?
+        .json::<Response>()
+        .await?
+        .list;
+
+    if definitions.is_empty() {
+        Ok(None)
+    } else {
+        Ok(Some(definitions.swap_remove(0)))
+    }
+}
+
+impl fmt::Display for Definition {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        writeln!(
+            f,
+            "[*{}*]({})",
+            markdown::escape(&self.word),
+            self.permalink
+        )?;
+        let definition = remove_brackets(self.definition.clone());
+        writeln!(f, "{}\n", markdown::escape(&definition))?;
+        let example = remove_brackets(self.example.clone());
+        writeln!(f, "_{}_\n", markdown::escape(&example))?;
+        writeln!(
+            f,
+            "by [{}]({}), {}",
+            markdown::escape(&self.author),
+            Url::parse_with_params(
+                "https://urbandictionary.com/author.php",
+                [("author", &self.author)]
+            )
+            .unwrap(),
+            markdown::escape(&self.written_on.format(DATETIME_FORMAT).unwrap())
+        )?;
+        write!(
+            f,
+            "\u{1F44D} {} \u{1F44E} {}",
+            self.thumbs_up, self.thumbs_down
+        )?;
+
+        Ok(())
+    }
+}
+
+fn remove_brackets<S: Into<String>>(text: S) -> String {
+    let mut text = text.into();
+    text.retain(|c| !"[]".contains(c));
+    text
+}
