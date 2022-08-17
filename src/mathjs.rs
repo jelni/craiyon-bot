b801ab4613e23a031c569303baa02048eb7cf8fa
jel <25802745+jelni@users.noreply.github.com>
2022-08-17 15:16:35 +0200

    Move API wrappers into single files

--- src/mathjs.rs
+++ src/mathjs.rs
@@ -1,4 +1,37 @@
-mod api;
-mod models;
+use serde::{Deserialize, Serialize};
 
-pub use api::*;
+#[derive(Serialize)]
+struct Payload {
+    pub expr: String,
+}
+
+#[derive(Deserialize)]
+struct Response {
+    pub result: String,
+}
+
+#[derive(Deserialize)]
+struct Error {
+    pub error: String,
+}
+
+use reqwest::StatusCode;
+
+pub async fn evaluate<S: Into<String>>(
+    http_client: reqwest::Client,
+    expr: S,
+) -> reqwest::Result<Result<String, String>> {
+    let response = http_client
+        .post("https://api.mathjs.org/v4/")
+        .json(&Payload { expr: expr.into() })
+        .send()
+        .await?;
+
+    let result = match response.status() {
+        StatusCode::OK => Ok(response.json::<Response>().await?.result),
+        StatusCode::BAD_REQUEST => Err(response.json::<Error>().await?.error),
+        _ => unreachable!(),
+    };
+
+    Ok(result)
+}
