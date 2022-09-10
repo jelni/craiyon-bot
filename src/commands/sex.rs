896e06644bba5a4e26ce76207e5f88b6067a0d59
ribone <40399432+ribxne@users.noreply.github.com>
2022-09-10 21:30:11 +0200

    Add more sex (#1)

--- src/commands/sex.rs
+++ src/commands/sex.rs
@@ -6,14 +6,18 @@ use tgbotapi::FileType;
 use super::Command;
 use crate::utils::Context;
 
-const SEX: &str = "CAACAgQAAxkBAAEX8npjHImztCnVUekWoGsQcoqzITtAiAACsQwAAhKVaVMIFeTFdsnn_CkE";
+const SEX: [&str; 2] = [
+    "CAACAgQAAxkBAAEX8npjHImztCnVUekWoGsQcoqzITtAiAACsQwAAhKVaVMIFeTFdsnn_CkE",
+    "CAACAgQAAxkBAAEX9DljHNrRW0S-xydtOOE7n9g4pFEixAACfwsAAhbWcFPn8gyFbHwwoykE",
+];
 
 pub struct Sex;
 
 #[async_trait]
 impl Command for Sex {
     async fn execute(&self, ctx: Context) -> Result<(), Box<dyn Error + Send + Sync>> {
-        ctx.send_sticker(FileType::FileID(SEX.to_string())).await?;
+        let question_mark = ctx.arguments.as_ref().map_or(false, |a| a.starts_with('?'));
+        ctx.send_sticker(FileType::FileID(SEX[usize::from(question_mark)].to_string())).await?;
 
         Ok(())
     }
