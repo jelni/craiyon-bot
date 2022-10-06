641ef98d0f676e8e56810766308f274ad3c369f3
JelNiSlaw <jelnislaw@gmail.com>
2022-10-06 22:51:15 +0200

    Save logs to file

--- src/logchamp.rs
+++ src/logchamp.rs
@@ -2,13 +2,28 @@
 //! joe: call it logchamp
 //! joe: please
 
+use std::fs::{File, OpenOptions};
+use std::io::Write;
+use std::sync::Mutex;
+
 use colored::{Color, Colorize};
-use log::{Level, Log, Metadata, Record};
+use log::{Level, LevelFilter, Log, Metadata, Record};
 use time::macros::format_description;
 
-static LOGGER: Logger = Logger;
+struct Logger {
+    file: Mutex<File>,
+}
 
-struct Logger;
+impl Logger {
+    fn new(filename: &str) -> Self {
+        log::set_max_level(LevelFilter::Debug);
+        Self {
+            file: Mutex::new(
+                OpenOptions::new().write(true).truncate(true).create(true).open(filename).unwrap(),
+            ),
+        }
+    }
+}
 
 impl Log for Logger {
     fn enabled(&self, metadata: &Metadata) -> bool {
@@ -24,34 +39,38 @@ impl Log for Logger {
             return;
         }
 
-        let color = match record.level() {
-            Level::Error => Color::Red,
-            Level::Warn => Color::Yellow,
-            Level::Info => Color::Blue,
-            Level::Debug => Color::Green,
-            Level::Trace => Color::Cyan,
+        let timestamp = time::OffsetDateTime::now_utc()
+            .format(format_description!("[year]-[month]-[day] [hour]:[minute]:[second]"))
+            .unwrap();
+
+        let (color, light_color) = match record.level() {
+            Level::Error => (Color::Red, Color::BrightRed),
+            Level::Warn => (Color::Yellow, Color::BrightYellow),
+            Level::Info => (Color::Cyan, Color::BrightCyan),
+            Level::Debug => (Color::Magenta, Color::BrightMagenta),
+            Level::Trace => (Color::Green, Color::BrightGreen),
         };
 
         let target = record.target().to_string();
         let level = record.level().as_str();
         let len: i32 = (target.len() + level.len()).try_into().unwrap();
-        let padding = " ".repeat((16 - len).max(0).try_into().unwrap());
+        let padding = " ".repeat((48 - len).max(0).try_into().unwrap());
 
         println!(
             "{} {padding}{} {} {}",
-            time::OffsetDateTime::now_utc()
-                .format(format_description!("[year]-[month]-[day] [hour]:[minute]:[second]"))
-                .unwrap()
-                .color(Color::BrightBlack),
+            timestamp.color(Color::BrightBlack),
             target.color(color),
-            level.color(color).bold(),
+            level.color(light_color),
             record.args()
         );
+
+        writeln!(self.file.lock().unwrap(), "{timestamp} [{target} {level}] {}", record.args())
+            .unwrap();
     }
 
     fn flush(&self) {}
 }
 
 pub fn init() {
-    log::set_logger(&LOGGER).unwrap();
+    log::set_boxed_logger(Box::new(Logger::new(".log"))).unwrap();
 }
