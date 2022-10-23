fe8a945333af46e43cdaecc2e414c81a01040d47
jel <25802745+jelni@users.noreply.github.com>
2022-10-23 23:55:18 +0200

    Improve /tts command

---
+++ src/apis/ivona.rs
@@ -0,0 +1,13 @@
+use reqwest::Url;
+
+pub async fn synthesize<S: AsRef<str>>(
+    http_client: reqwest::Client,
+    text: S,
+    voice: &str,
+) -> reqwest::Result<Vec<u8>> {
+    let mut url =
+        Url::parse_with_params("https://ivona.sakamoto.pl/ivonaapi", [("text", text.as_ref())])
+            .unwrap();
+    url.path_segments_mut().unwrap().push(voice);
+    Ok(http_client.get(url).send().await?.bytes().await?.to_vec())
+}
