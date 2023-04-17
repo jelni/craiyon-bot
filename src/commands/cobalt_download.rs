79eacb14a928f723d549cc2efecb23d870788cd6
JelNiSlaw <jelnislaw@gmail.com>
2023-04-17 16:11:29 +0200

    accept reply arguments in more commands

--- src/commands/cobalt_download.rs
+++ src/commands/cobalt_download.rs
@@ -6,7 +6,7 @@ use tdlib::types::{InputFileLocal, InputMessageDocument};
 use super::{CommandResult, CommandTrait};
 use crate::apis::cobalt;
 use crate::utilities::command_context::CommandContext;
-use crate::utilities::convert_argument::{ConvertArgument, StringGreedy};
+use crate::utilities::convert_argument::{ConvertArgument, StringGreedyOrReply};
 use crate::utilities::file_download::{DownloadError, NetworkFile};
 use crate::utilities::telegram_utils;
 
@@ -23,7 +23,7 @@ impl CommandTrait for CobaltDownload {
     }
 
     async fn execute(&self, ctx: &CommandContext, arguments: String) -> CommandResult {
-        let StringGreedy(media_url) = ConvertArgument::convert(ctx, &arguments).await?.0;
+        let StringGreedyOrReply(media_url) = ConvertArgument::convert(ctx, &arguments).await?.0;
 
         ctx.send_typing().await?;
         let urls = cobalt::query(ctx.http_client.clone(), &media_url)
