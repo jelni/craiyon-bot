ad1f9eb1cf4806d22fb7ce153969b62f4e3ae8cc
pxseu <57842793+pxseu@users.noreply.github.com>
2023-12-21 00:54:47 +0100

    add nitro promotion command via opera gx

---
+++ src/apis/opera_gx.rs
@@ -0,0 +1,33 @@
+use reqwest::header::ACCEPT;
+use serde::{Deserialize, Serialize};
+
+use crate::commands::CommandError;
+use crate::utilities::api_utils::DetectServerError;
+
+#[derive(Serialize)]
+#[serde(rename_all = "camelCase")]
+struct Payload<'a> {
+    partner_user_id: &'a str,
+}
+
+#[derive(Deserialize)]
+struct Response {
+    token: String,
+}
+
+pub async fn generate(http_client: reqwest::Client) -> Result<String, CommandError> {
+    let response = http_client
+        .post("https://api.discord.gx.games/v1/direct-fulfillment")
+        .json(&Payload {
+            partner_user_id: "510429d266a6a5e2374f80a2942c7cfe7fc317b21b7b31b06d4a54a0287aacb8",
+        })
+        .header(ACCEPT, "application/json")
+        .send()
+        .await?
+        .server_error()?
+        .json::<Response>()
+        .await?
+        .token;
+
+    Ok(format!("https://discord.com/billing/partner-promotions/1180231712274387115/{token}"))
+}
---
+++ src/commands/nitro.rs
@@ -0,0 +1,26 @@
+use async_trait::async_trait;
+
+use super::{CommandResult, CommandTrait};
+use crate::apis::opera_gx;
+use crate::utilities::command_context::CommandContext;
+
+pub struct Nitro;
+
+#[async_trait]
+impl CommandTrait for Nitro {
+    fn command_names(&self) -> &[&str] {
+        &["nitro"]
+    }
+
+    fn description(&self) -> Option<&'static str> {
+        Some("Generates a Discord Nitro promotional link")
+    }
+
+    async fn execute(&self, ctx: &CommandContext, _: String) -> CommandResult {
+        let link = opera_gx::generate(ctx.http_client()).await?;
+
+        ctx.reply(link).await?;
+
+        Ok(())
+    }
+}
