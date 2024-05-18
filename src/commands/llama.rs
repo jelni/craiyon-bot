d8805a7d262ad8e481e75c9a2451f109f6eaeab4
Alan Klas <alan@alanklas.com>
2024-05-19 00:44:48 +0200

    inline some parameters

--- src/commands/llama.rs
+++ src/commands/llama.rs
@@ -1,12 +1,9 @@
-use std::fmt::Write;
-
 use async_trait::async_trait;
 
 use tdlib::types::FormattedText;
 use tdlib::{enums, functions};
 
 use super::{CommandError, CommandResult, CommandTrait};
-use crate::apis::makersuite::Part;
 use crate::apis::openai;
 use crate::utilities::command_context::CommandContext;
 use crate::utilities::convert_argument::{ConvertArgument, StringGreedyOrReply};
@@ -31,41 +28,38 @@ impl CommandTrait for Llama {
     async fn execute(&self, ctx: &CommandContext, arguments: String) -> CommandResult {
         let prompt: Option<StringGreedyOrReply> = Option::<StringGreedyOrReply>::convert(ctx, &arguments).await?.0;
         let prompt = prompt.unwrap().0;
-
+    
         ctx.send_typing().await?;
-
-        let model = "llama3-70b-8192";
+        
         let token = std::env::var("GROQ_API_KEY").unwrap();
-
-        let http_client = ctx.bot_state.http_client.clone();
-
+    
         let response = openai::chat_completion(
-            http_client,
+            ctx.bot_state.http_client.clone(),
             &token,
             "https://api.groq.com/openai/v1",
-            model,
+            "llama3-70b-8192",
             &prompt,
             None,
         )
         .await;
-
+    
         let response = response?;
-
+    
         match response {
             Ok(response) => {
                 let text = response.choices.into_iter().next().unwrap().message.content;
-
+    
                 let enums::FormattedText::FormattedText(formatted_text) =
                     functions::parse_markdown(
                         FormattedText { text, ..Default::default() },
                         ctx.client_id,
                     )
                     .await?;
-
+    
                 let unsent_message = ctx.reply_formatted_text(formatted_text).await?;
                 let _message =
                     ctx.bot_state.message_queue.wait_for_message(unsent_message.id).await?;
-
+    
                 Ok(())
             }
             Err(e) => Err(CommandError::Custom(e.message)),
