55d5f3b5da47cc628814e2f9a6f7958a3e66843c
Alan Klas <alan@alanklas.com>
2024-07-24 00:01:31 +0200

    petittube change

--- src/apis/petittube.rs
+++ src/apis/petittube.rs
@@ -1,13 +1,13 @@
 use crate::{commands::CommandError, utilities::api_utils::DetectServerError};
 
+const YOUTUBE_EMBED: &str = "https://www.youtube.com/embed/";
+
 pub async fn random_video(http_client: &reqwest::Client) -> Result<String, CommandError> {
     // This requires web scraping, because the site doesn't have an API
     let body = http_client.get("https://petittube.com").send().await?.server_error()?.text().await?;
-    let video_split: Vec<&str> = body
-        .split("https://www.youtube.com/embed/")
-        .collect::<Vec<&str>>();
-    let identifier = video_split[1].split('?').collect::<Vec<&str>>()[0];
-    let url = format!("https://youtu.be/{}", identifier); 
+    let index = body.find(YOUTUBE_EMBED).ok_or(CommandError::Custom("YOUTUBE_EMBED not found".into()))?;
+    let identifier: String = body[index + YOUTUBE_EMBED.len()..].chars().take_while(|&c| c != '?').collect();
+    let url = format!("https://youtu.be/{}", identifier);
 
     Ok(url)
 }
