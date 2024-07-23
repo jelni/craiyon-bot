dc9a3153c573158b8a8055e5c88ce7cda6779f9e
Alan Klas <alan@alanklas.com>
2024-07-23 23:55:43 +0200

    oops

--- src/apis/petittube.rs
+++ src/apis/petittube.rs
@@ -2,8 +2,7 @@ use crate::{commands::CommandError, utilities::api_utils::DetectServerError};
 
 pub async fn random_video(http_client: &reqwest::Client) -> Result<String, CommandError> {
     // This requires web scraping, because the site doesn't have an API
-    let url = "https://petittube.com";
-    let body = http_client.get(url).send().await?.server_error()?.text().await?;
+    let body = http_client.get("https://petittube.com").send().await?.server_error()?.text().await?;
     let video_split: Vec<&str> = body
         .split("https://www.youtube.com/embed/")
         .collect::<Vec<&str>>();
