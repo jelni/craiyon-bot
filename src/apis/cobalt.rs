d3a0e54dc784aed7da590a40b8a516762c7604eb
Alan Klas <alan@alanklas.com>
2024-10-20 23:06:57 +0200

    fixed cobalt by implementing self-host

--- src/apis/cobalt.rs
+++ src/apis/cobalt.rs
@@ -1,18 +1,16 @@
-use std::collections::HashMap;
-
 use reqwest::header::ACCEPT;
 use serde::{Deserialize, Serialize};
 
-use crate::commands::CommandError;
+use crate::commands::cobalt_download::COBALT_URL;
 use crate::utilities::api_utils::{DetectServerError, ServerError};
 
 #[derive(Serialize)]
 #[serde(rename_all = "camelCase")]
 struct Payload<'a> {
     url: &'a str,
-    v_quality: &'a str,
-    a_format: &'a str,
-    is_audio_only: bool,
+    video_quality: &'a str,
+    audio_format: &'a str,
+    download_mode: &'a str,
     #[serde(rename = "tiktokH265")]
     tiktok_h265: bool,
 }
@@ -34,6 +32,7 @@ enum Status {
     Success,
     Error,
     RateLimit,
+    Tunnel,
 }
 
 #[derive(Deserialize)]
@@ -49,17 +48,16 @@ pub enum Error {
 
 pub async fn query(
     http_client: reqwest::Client,
-    domain: &str,
     url: &str,
     audio_only: bool,
 ) -> Result<Vec<String>, Error> {
     let response = http_client
-        .post(format!("https://{domain}/api/json"))
+        .post(COBALT_URL)
         .json(&Payload {
             url,
-            v_quality: "1080",
-            a_format: "best",
-            is_audio_only: audio_only,
+            video_quality: "1080",
+            audio_format: "best",
+            download_mode: if audio_only { "audio" } else { "auto" },
             tiktok_h265: true,
         })
         .header(ACCEPT, "application/json")
@@ -73,84 +71,43 @@ pub async fn query(
         .map_err(Error::Network)?;
 
     match response.status {
-        Status::Stream | Status::Redirect => Ok(vec![response.url.unwrap()]),
+        Status::Stream | Status::Redirect | Status::Tunnel => Ok(vec![response.url.unwrap()]),
         Status::Picker => Ok(response.picker.unwrap().into_iter().map(|i| i.url).collect()),
         Status::Success | Status::Error | Status::RateLimit => {
-            Err(Error::Cobalt(response.text.unwrap()))
+            Err(Error::Cobalt(response.text.unwrap_or_else(String::new)))
         }
     }
 }
 
-#[derive(Deserialize)]
-pub struct Instance {
-    pub api_online: bool,
-    pub services: HashMap<Service, bool>,
-    pub score: f32,
-    pub protocol: String,
-    pub api: String,
-}
 
-#[derive(Clone, Copy, PartialEq, Eq, Hash, Deserialize)]
-#[serde(rename_all = "snake_case")]
-pub enum Service {
-    Youtube,
-    Rutube,
-    Tumblr,
-    Bilibili,
-    Pinterest,
-    Instagram,
-    Soundcloud,
-    YoutubeMusic,
-    Odnoklassniki,
-    Dailymotion,
-    Twitter,
-    Loom,
-    Vimeo,
-    Streamable,
-    Vk,
-    Tiktok,
-    Reddit,
-    TwitchClips,
-    YoutubeShorts,
-    Vine,
-    #[serde(other)]
-    Unknown,
-}
+// #[derive(Deserialize)]
+// pub struct CobaltInfo {
+//     pub cobalt: Cobalt,
+//     pub git: Git,
+// }
 
-impl Service {
-    pub const fn name(self) -> Option<&'static str> {
-        match self {
-            Self::Youtube => Some("YouTube"),
-            Self::Rutube => Some("RUTUBE"),
-            Self::Tumblr => Some("Tumblr"),
-            Self::Bilibili => Some("BiliBili"),
-            Self::Pinterest => Some("Pinterest"),
-            Self::Instagram => Some("Instagram"),
-            Self::Soundcloud => Some("SoundCloud"),
-            Self::YoutubeMusic => Some("YouTube Music"),
-            Self::Odnoklassniki => Some("Odnoklassniki"),
-            Self::Dailymotion => Some("Dailymotion"),
-            Self::Twitter => Some("Twitter"),
-            Self::Loom => Some("Loom"),
-            Self::Vimeo => Some("Vimeo"),
-            Self::Streamable => Some("Streamable"),
-            Self::Vk => Some("VK"),
-            Self::Tiktok => Some("TikTok"),
-            Self::Reddit => Some("Reddit"),
-            Self::TwitchClips => Some("Twitch (Clips)"),
-            Self::YoutubeShorts => Some("YouTube (Shorts)"),
-            Self::Vine => Some("Vine"),
-            Self::Unknown => None,
-        }
-    }
-}
+// #[derive(Deserialize)]
+// pub struct Cobalt {
+//     pub version: String,
+//     pub url: String,
+//     pub start_time: String,
+//     pub duration_limit: u64,
+//     pub services: Vec<String>,
+// }
 
-pub async fn instances(http_client: reqwest::Client) -> Result<Vec<Instance>, CommandError> {
-    let response = http_client
-        .get("https://instances.hyper.lol/instances.json")
-        .send()
-        .await?
-        .server_error()?;
+// #[derive(Deserialize)]
+// pub struct Git {
+//     pub branch: String,
+//     pub commit: String,
+//     pub remote: String,
+// }
 
-    Ok(response.json().await?)
-}
+// pub async fn info(http_client: reqwest::Client) -> Result<CobaltInfo, CommandError> {
+//     let response = http_client
+//         .get(COBALT_URL)
+//         .send()
+//         .await?
+//         .server_error()?;
+
+//     Ok(response.json().await?)
+// }
--- src/commands/cobalt_download.rs
+++ src/commands/cobalt_download.rs
@@ -1,56 +1,20 @@
 use std::borrow::Cow;
-
 use async_trait::async_trait;
-use rand::seq::IteratorRandom;
 use tdlib::enums::{InputFile, InputMessageContent, InputMessageReplyTo, Messages};
 use tdlib::functions;
 use tdlib::types::{
     InputFileLocal, InputMessageAudio, InputMessageDocument, InputMessageReplyToMessage,
     InputMessageVideo,
 };
-
 use super::{CommandError, CommandResult, CommandTrait};
-use crate::apis::cobalt::{self, Error, Instance, Service};
+use crate::apis::cobalt::{self, Error};
 use crate::utilities::command_context::CommandContext;
 use crate::utilities::convert_argument::{ConvertArgument, StringGreedyOrReply};
 use crate::utilities::file_download::{DownloadError, NetworkFile};
 use crate::utilities::message_entities::{self, ToEntity};
 use crate::utilities::telegram_utils;
 
-const MAIN_INSTANCE: &str = "api.cobalt.tools";
-
-const SERVICE_URLS: &[(Service, &[&str])] = &[
-    (Service::Youtube, &["youtu.be/", "youtube.com/watch?", "www.youtube.com/watch?"]),
-    (Service::Rutube, &["rutube.ru/video/"]),
-    (Service::Tumblr, &["www.tumblr.com/", "tumblr.com/"]),
-    (Service::Bilibili, &["www.bilibili.com/video/", "bilibili.com/video/"]),
-    (Service::Pinterest, &["pinterest.com/pin/"]),
-    (
-        Service::Instagram,
-        &[
-            "www.instagram.com/p/",
-            "instagram.com/p/",
-            "www.instagram.com/reels/",
-            "instagram.com/reels/",
-            "www.instagram.com/reel/",
-            "instagram.com/reel/",
-        ],
-    ),
-    (Service::Soundcloud, &["soundcloud.com/"]),
-    (Service::YoutubeMusic, &["music.youtube.com/watch?"]),
-    (Service::Odnoklassniki, &["ok.ru/"]),
-    (Service::Dailymotion, &["www.dailymotion.com/video/", "dailymotion.com/video/"]),
-    (Service::Twitter, &["x.com/", "twitter.com/"]),
-    (Service::Loom, &["www.loom.com/share/", "loom.com/share/"]),
-    (Service::Vimeo, &["vimeo.com/"]),
-    (Service::Streamable, &["streamable.com/"]),
-    (Service::Vk, &["vk.com/video-", "vk.com/clip-"]),
-    (Service::Tiktok, &["www.tiktok.com/", "tiktok.com/", "vm.tiktok.com/"]),
-    (Service::Reddit, &["www.reddit.com/", "reddit.com/"]),
-    (Service::TwitchClips, &["clips.twitch.tv/"]),
-    (Service::YoutubeShorts, &["www.youtube.com/shorts/", "youtube.com/shorts/"]),
-    (Service::Vine, &["vine.co/v/"]),
-];
+pub const COBALT_URL: &str = "http://localhost:9000/";
 
 pub struct CobaltDownload {
     audio_only: bool,
@@ -88,7 +52,6 @@ impl CommandTrait for CobaltDownload {
 
     async fn execute(&self, ctx: &CommandContext, arguments: String) -> CommandResult {
         let StringGreedyOrReply(media_url) = ConvertArgument::convert(ctx, &arguments).await?.0;
-
         ctx.send_typing().await?;
 
         let (instance, mut urls) =
@@ -119,8 +82,8 @@ impl CommandTrait for CobaltDownload {
                 Ok(file) => files.push(file),
                 Err(err) => match err {
                     DownloadError::RequestError(err) => {
-                        log::warn!("cobalt download failed: {err}");
-                        Err(format!("≫ cobalt download failed: {}", err.without_url()))?;
+                        log::warn!("≫cobalt download failed: {err}");
+                        Err(format!("≫cobalt download failed: {}", err.without_url()))?;
                     }
                     DownloadError::FilesystemError => {
                         Err("failed to save the file to the hard drive.")?;
@@ -146,117 +109,6 @@ impl CommandTrait for CobaltDownload {
     }
 }
 
-async fn get_urls(
-    http_client: reqwest::Client,
-    url: &str,
-    audio_only: bool,
-) -> Result<(Cow<str>, Vec<String>), CommandError> {
-    match cobalt::query(http_client.clone(), MAIN_INSTANCE, url, audio_only).await {
-        Ok(urls) => Ok((Cow::Borrowed(MAIN_INSTANCE), urls)),
-        Err(err) => {
-            let mut instances = cobalt::instances(http_client.clone()).await?;
-
-            instances.retain(|instance| {
-                instance.api_online && instance.protocol == "https" && instance.api != MAIN_INSTANCE
-            });
-
-            let service = filter_instances_for_url(url, &mut instances);
-
-            let instance =
-                instances.into_iter().choose(&mut rand::thread_rng()).ok_or_else(|| {
-                    service.and_then(Service::name).map_or_else(
-                        || {
-                            CommandError::Custom(
-                                "no fully healthy ≫ cobalt instances are online.".into(),
-                            )
-                        },
-                        |service_name| {
-                            CommandError::Custom(format!(
-                                "none of the online ≫ cobalt instances support {service_name}."
-                            ))
-                        },
-                    )
-                })?;
-
-            if let Ok(urls) = cobalt::query(http_client, &instance.api, url, audio_only).await {
-                Ok((Cow::Owned(instance.api), urls))
-            } else {
-                Err(match err {
-                    Error::Cobalt(err) => err.into(),
-                    Error::Server(err) => err.into(),
-                    Error::Network(err) => err.into(),
-                })
-            }
-        }
-    }
-}
-
-fn filter_instances_for_url(url: &str, instances: &mut Vec<Instance>) -> Option<Service> {
-    let url =
-        url.strip_prefix("https://").unwrap_or_else(|| url.strip_prefix("http://").unwrap_or(url));
-
-    let service = SERVICE_URLS
-        .iter()
-        .find(|(_, service_urls)| {
-            service_urls.iter().any(|service_url| url.starts_with(service_url))
-        })
-        .map(|service| service.0);
-
-    if let Some(service) = service {
-        instances.retain(|instance| instance.services.get(&service).copied().unwrap_or_default());
-    } else {
-        #[allow(clippy::float_cmp)]
-        instances.retain(|instance| instance.score == 100.);
-    };
-
-    service
-}
-
-fn get_message_content(file: &NetworkFile) -> InputMessageContent {
-    let input_file =
-        InputFile::Local(InputFileLocal { path: file.file_path.to_str().unwrap().into() });
-
-    if file.content_type.as_ref().is_some_and(|content_type| content_type == "video/mp4")
-        || file.file_path.extension().is_some_and(|extension| extension.eq_ignore_ascii_case("mp4"))
-    {
-        InputMessageContent::InputMessageVideo(InputMessageVideo {
-            video: input_file,
-            thumbnail: None,
-            added_sticker_file_ids: Vec::new(),
-            duration: 0,
-            width: 0,
-            height: 0,
-            supports_streaming: true,
-            caption: None,
-            show_caption_above_media: false,
-            self_destruct_type: None,
-            has_spoiler: false,
-        })
-    } else if file
-        .content_type
-        .as_ref()
-        .is_some_and(|content_type| ["audio/mpeg", "audio/webm"].contains(&content_type.as_str()))
-        || file.file_path.extension().is_some_and(|extension| {
-            extension.eq_ignore_ascii_case("mp3") || extension.eq_ignore_ascii_case("opus")
-        })
-    {
-        InputMessageContent::InputMessageAudio(InputMessageAudio {
-            audio: input_file,
-            album_cover_thumbnail: None,
-            duration: 0,
-            title: String::new(),
-            performer: String::new(),
-            caption: None,
-        })
-    } else {
-        InputMessageContent::InputMessageDocument(InputMessageDocument {
-            document: input_file,
-            thumbnail: None,
-            disable_content_type_detection: false,
-            caption: None,
-        })
-    }
-}
 
 async fn send_files(ctx: &CommandContext, files: &[NetworkFile]) -> CommandResult {
     if let [file] = files {
@@ -265,7 +117,7 @@ async fn send_files(ctx: &CommandContext, files: &[NetworkFile]) -> CommandResul
             .wait_for_message(
                 ctx.reply_custom(
                     get_message_content(file),
-                    Some(telegram_utils::donate_markup("≫ cobalt", "https://boosty.to/wukko")),
+                    Some(telegram_utils::donate_markup("≫cobalt", "https://boosty.to/wukko")),
                 )
                 .await?
                 .id,
@@ -314,4 +166,71 @@ async fn send_files(ctx: &CommandContext, files: &[NetworkFile]) -> CommandResul
     }
 
     Ok(())
+}
+
+fn get_message_content(file: &NetworkFile) -> InputMessageContent {
+    let input_file =
+        InputFile::Local(InputFileLocal { path: file.file_path.to_str().unwrap().into() });
+
+    if file.content_type.as_ref().is_some_and(|content_type| content_type == "video/mp4")
+        || file.file_path.extension().is_some_and(|extension| extension.eq_ignore_ascii_case("mp4"))
+    {
+        InputMessageContent::InputMessageVideo(InputMessageVideo {
+            video: input_file,
+            thumbnail: None,
+            added_sticker_file_ids: Vec::new(),
+            duration: 0,
+            width: 0,
+            height: 0,
+            supports_streaming: true,
+            caption: None,
+            show_caption_above_media: false,
+            self_destruct_type: None,
+            has_spoiler: false,
+        })
+    } else if file
+        .content_type
+        .as_ref()
+        .is_some_and(|content_type| ["audio/mpeg", "audio/webm"].contains(&content_type.as_str()))
+        || file.file_path.extension().is_some_and(|extension| {
+            extension.eq_ignore_ascii_case("mp3") || extension.eq_ignore_ascii_case("opus")
+        })
+    {
+        InputMessageContent::InputMessageAudio(InputMessageAudio {
+            audio: input_file,
+            album_cover_thumbnail: None,
+            duration: 0,
+            title: String::new(),
+            performer: String::new(),
+            caption: None,
+        })
+    } else {
+        InputMessageContent::InputMessageDocument(InputMessageDocument {
+            document: input_file,
+            thumbnail: None,
+            disable_content_type_detection: false,
+            caption: None,
+        })
+    }
+}
+
+async fn get_urls(
+    http_client: reqwest::Client,
+    url: &str,
+    audio_only: bool,
+) -> Result<(Cow<str>, Vec<String>), CommandError> {
+    match cobalt::query(http_client.clone(), url, audio_only).await {
+        Ok(urls) => Ok((Cow::Borrowed(COBALT_URL), urls)),
+        Err(err) => {
+            // Try querying the selected instance
+            cobalt::query(http_client, url, audio_only)
+                .await
+                .map(|urls| (Cow::Borrowed(COBALT_URL), urls))
+                .map_err(|_e| match err {
+                    Error::Cobalt(err) => err.into(),
+                    Error::Server(err) => err.into(),
+                    Error::Network(err) => err.into(),
+                })
+        }
+    }
 }
