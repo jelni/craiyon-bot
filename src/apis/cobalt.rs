4df6124be52564a75c7b051af49704db94d30442
DuckyBlender <alan@alanklas.com>
2024-10-21 10:49:01 +0200

    Revert "fixed cobalt by implementing self-host"
    This reverts commit b63bca7509f33b12ecb050485b9f56c769c70cb6.

--- src/apis/cobalt.rs
+++ src/apis/cobalt.rs
@@ -1,16 +1,18 @@
+use std::collections::HashMap;
+
 use reqwest::header::ACCEPT;
 use serde::{Deserialize, Serialize};
 
-use crate::commands::cobalt_download::COBALT_URL;
+use crate::commands::CommandError;
 use crate::utilities::api_utils::{DetectServerError, ServerError};
 
 #[derive(Serialize)]
 #[serde(rename_all = "camelCase")]
 struct Payload<'a> {
     url: &'a str,
-    video_quality: &'a str,
-    audio_format: &'a str,
-    download_mode: &'a str,
+    v_quality: &'a str,
+    a_format: &'a str,
+    is_audio_only: bool,
     #[serde(rename = "tiktokH265")]
     tiktok_h265: bool,
 }
@@ -32,7 +34,6 @@ enum Status {
     Success,
     Error,
     RateLimit,
-    Tunnel,
 }
 
 #[derive(Deserialize)]
@@ -48,16 +49,17 @@ pub enum Error {
 
 pub async fn query(
     http_client: reqwest::Client,
+    domain: &str,
     url: &str,
     audio_only: bool,
 ) -> Result<Vec<String>, Error> {
     let response = http_client
-        .post(COBALT_URL)
+        .post(format!("https://{domain}/api/json"))
         .json(&Payload {
             url,
-            video_quality: "1080",
-            audio_format: "best",
-            download_mode: if audio_only { "audio" } else { "auto" },
+            v_quality: "1080",
+            a_format: "best",
+            is_audio_only: audio_only,
             tiktok_h265: true,
         })
         .header(ACCEPT, "application/json")
@@ -71,43 +73,84 @@ pub async fn query(
         .map_err(Error::Network)?;
 
     match response.status {
-        Status::Stream | Status::Redirect | Status::Tunnel => Ok(vec![response.url.unwrap()]),
+        Status::Stream | Status::Redirect => Ok(vec![response.url.unwrap()]),
         Status::Picker => Ok(response.picker.unwrap().into_iter().map(|i| i.url).collect()),
         Status::Success | Status::Error | Status::RateLimit => {
-            Err(Error::Cobalt(response.text.unwrap_or_else(String::new)))
+            Err(Error::Cobalt(response.text.unwrap()))
         }
     }
 }
 
+#[derive(Deserialize)]
+pub struct Instance {
+    pub api_online: bool,
+    pub services: HashMap<Service, bool>,
+    pub score: f32,
+    pub protocol: String,
+    pub api: String,
+}
 
-// #[derive(Deserialize)]
-// pub struct CobaltInfo {
-//     pub cobalt: Cobalt,
-//     pub git: Git,
-// }
+#[derive(Clone, Copy, PartialEq, Eq, Hash, Deserialize)]
+#[serde(rename_all = "snake_case")]
+pub enum Service {
+    Youtube,
+    Rutube,
+    Tumblr,
+    Bilibili,
+    Pinterest,
+    Instagram,
+    Soundcloud,
+    YoutubeMusic,
+    Odnoklassniki,
+    Dailymotion,
+    Twitter,
+    Loom,
+    Vimeo,
+    Streamable,
+    Vk,
+    Tiktok,
+    Reddit,
+    TwitchClips,
+    YoutubeShorts,
+    Vine,
+    #[serde(other)]
+    Unknown,
+}
 
-// #[derive(Deserialize)]
-// pub struct Cobalt {
-//     pub version: String,
-//     pub url: String,
-//     pub start_time: String,
-//     pub duration_limit: u64,
-//     pub services: Vec<String>,
-// }
+impl Service {
+    pub const fn name(self) -> Option<&'static str> {
+        match self {
+            Self::Youtube => Some("YouTube"),
+            Self::Rutube => Some("RUTUBE"),
+            Self::Tumblr => Some("Tumblr"),
+            Self::Bilibili => Some("BiliBili"),
+            Self::Pinterest => Some("Pinterest"),
+            Self::Instagram => Some("Instagram"),
+            Self::Soundcloud => Some("SoundCloud"),
+            Self::YoutubeMusic => Some("YouTube Music"),
+            Self::Odnoklassniki => Some("Odnoklassniki"),
+            Self::Dailymotion => Some("Dailymotion"),
+            Self::Twitter => Some("Twitter"),
+            Self::Loom => Some("Loom"),
+            Self::Vimeo => Some("Vimeo"),
+            Self::Streamable => Some("Streamable"),
+            Self::Vk => Some("VK"),
+            Self::Tiktok => Some("TikTok"),
+            Self::Reddit => Some("Reddit"),
+            Self::TwitchClips => Some("Twitch (Clips)"),
+            Self::YoutubeShorts => Some("YouTube (Shorts)"),
+            Self::Vine => Some("Vine"),
+            Self::Unknown => None,
+        }
+    }
+}
 
-// #[derive(Deserialize)]
-// pub struct Git {
-//     pub branch: String,
-//     pub commit: String,
-//     pub remote: String,
-// }
+pub async fn instances(http_client: reqwest::Client) -> Result<Vec<Instance>, CommandError> {
+    let response = http_client
+        .get("https://instances.hyper.lol/instances.json")
+        .send()
+        .await?
+        .server_error()?;
 
-// pub async fn info(http_client: reqwest::Client) -> Result<CobaltInfo, CommandError> {
-//     let response = http_client
-//         .get(COBALT_URL)
-//         .send()
-//         .await?
-//         .server_error()?;
-
-//     Ok(response.json().await?)
-// }
+    Ok(response.json().await?)
+}
