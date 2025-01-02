e9971bafde21de54737db7da08f7db7013fa5631
jel <25802745+jelni@users.noreply.github.com>
2025-01-03 00:50:55 +0100

    update /start command

--- src/commands/start.rs
+++ src/commands/start.rs
@@ -14,10 +14,32 @@ impl CommandTrait for Start {
 
     async fn execute(&self, ctx: &CommandContext, _: String) -> CommandResult {
         ctx.reply_formatted_text(message_entities::formatted_text(vec![
-            "use the /craiyon_art command to generate images.\n".text(),
-            "example:".bold(),
-            " ".text(),
-            "/craiyon_art crayons in a box".code(),
+            concat!(
+                "hello! this bot contains many useful and fun commands like:\n",
+                "• /gemini (/g), /gemini2 (/g2) – talk to Google Gemini\n",
+                "• /cobalt_download (/dl), /cobalt_download_audio (/dla) – download media using ",
+            )
+            .text(),
+            "≫ cobalt".text_url("https://cobalt.tools/"),
+            "\n• /yt_dlp (/yt), /yt_dlp_audio (/yta) – download media using ".text(),
+            "yt-dlp".text_url("https://github.com/yt-dlp/yt-dlp"),
+            concat!(
+                "\n• /translate (/tr) – translate text using Google Translate\n",
+                "• /screenshot (/ss) – screenshot websites\n",
+                "• /urbandictionary (/ud) – get slang term definitions\n",
+                "• /charinfo (/ch) – see Unicode character names\n",
+                "and more!\n\n",
+                "you can reply to other messages ",
+                "or quote their parts to provide arguments. ",
+                "this is my open-source hobby project, made using ",
+            )
+            .text(),
+            "Rust".text_url("https://www.rust-lang.org/"),
+            " and ".text(),
+            "TDLib".text_url("https://core.telegram.org/tdlib"),
+            ". not everything will always work. star the repository on ".text(),
+            "GitHub".text_url("https://github.com/jelni/craiyon-bot"),
+            "!\n- @zuzia".text(),
         ]))
         .await?;
 
