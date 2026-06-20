620405dbfdf2d683e942ee1279bcf7b38a6e070d
jel <git@jel.gay>
2026-06-20 18:14:44 +0200

    `flatten` optional details

--- src/apis/google_aistudio.rs
+++ src/apis/google_aistudio.rs
@@ -68,11 +68,9 @@ pub enum Details {
 
 impl fmt::Display for Error {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        if let Some(retry_delay) = self.details.as_ref().and_then(|details| {
-            details.iter().find_map(|detail| match detail {
-                Details::RetryInfo { retry_delay } => Some(retry_delay),
-                Details::Other => None,
-            })
+        if let Some(retry_delay) = self.details.iter().flatten().find_map(|detail| match detail {
+            Details::RetryInfo { retry_delay } => Some(retry_delay),
+            Details::Other => None,
         }) {
             return write!(f, "i was rate-limited by Google. try again in {retry_delay}.");
         }
